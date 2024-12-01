1f7242fcea2f88bdced8eeaff59b13cb92269ba6
Molive <moliveofscratch@gmail.com>
2024-12-01 23:04:43 +0000

    Day 1

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -1,59 +1,27 @@
+use itertools::Itertools;
+
 #[macro_use]
 mod help;
 fn main() {
-    let data = input_to_str_iterator!("1");
+    let data = input_to_str_iterator!("1").map(|f| {
+        let mut maps = f.split_whitespace().map(|v| v.parse::<i32>().unwrap());
+        (maps.next().unwrap(), maps.next().unwrap())
+    }).collect::<Vec<_>>();
 
-    part1!(data
-        .map(|line| {
-            let chars = line.chars();
-            let mut forwards = chars.clone();
-            let mut backwards = chars.rev().clone();
-            let first = forwards.find(char::is_ascii_digit).unwrap();
-            let last = backwards.find(char::is_ascii_digit).unwrap();
-            let both = format!("{}{}", first, last);
-            let number: usize = both.parse().unwrap();
-            number
+    let data_left = data.iter().map(|f| f.0).sorted().collect::<Vec<_>>();
+    let data_right = data.iter().map(|f| f.1).sorted().collect::<Vec<_>>();
+
+    part1!(data_left.iter().copied().zip(data_right.iter().copied())
+        .map(|(left, right)| {
+            let diff = (left - right).abs();
+            diff
         })
-        .sum::<usize>());
+        .sum::<i32>());
 
-    let data = input_to_str_iterator!("1");
-
-    part2!(data
-        .map(|line| {
-            let chars = line.chars();
-            let mut forwards = chars.clone();
-            let mut backwards = chars.clone().rev();
-            let mut first = {
-                let p = forwards.position(|c| c.is_ascii_digit()).unwrap();
-                (p, chars.clone().nth(p).unwrap().to_string())
-            };
-            let mut last = {
-                let p = backwards.position(|c| c.is_ascii_digit()).unwrap();
-                (
-                    line.len() - p - 1,
-                    chars.clone().nth_back(p).unwrap().to_string(),
-                )
-            };
-            for number in [
-                "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
-            ]
-            .iter()
-            .enumerate()
-            {
-                if let Some(n) = line.match_indices(number.1).next() {
-                    if n.0 < first.0 {
-                        first = (n.0, (number.0 + 1).to_string());
-                    }
-                }
-                if let Some(n) = line.match_indices(number.1).last() {
-                    if n.0 > last.0 {
-                        last = (n.0, (number.0 + 1).to_string());
-                    }
-                }
-            }
-            let both = format!("{}{}", first.1, last.1);
-            let number: usize = both.parse().unwrap();
-            number
+    part2!(data_left.iter().copied()
+        .map(|left| {
+            let right = data_right.iter().copied().filter(|v| *v == left).count();
+            left * (right as i32)
         })
-        .sum::<usize>());
+        .sum::<i32>());
 }
